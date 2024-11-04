05e2859c5f2a0db65ff36667f865ec1b0a8d4fbb
dashawnpaige27 <dap6024@psu.edu>
2024-11-03 23:36:30 -0500

    changes

--- hax-search.js
+++ hax-search.js
@@ -8,12 +8,11 @@ import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
 
 /**
  * `hax-search`
- * 
+ *
  * @demo index.html
  * @element hax-search
  */
 export class HaxSearch extends DDDSuper(I18NMixin(LitElement)) {
-
   static get tag() {
     return "hax-search";
   }
@@ -21,16 +20,16 @@ export class HaxSearch extends DDDSuper(I18NMixin(LitElement)) {
   constructor() {
     super();
     this.title = "";
-    this.t = this.t || {};
+    this.siteUrl = "";
+    this.results = [];
     this.t = {
-      ...this.t,
       title: "Title",
     };
+
     this.registerLocalization({
       context: this,
       localesPath:
-        new URL("./locales/hax-search.ar.json", import.meta.url).href +
-        "/../",
+        new URL("./locales/hax-search.ar.json", import.meta.url).href + "/../",
       locales: ["ar", "es", "hi", "zh"],
     });
   }
@@ -40,36 +39,81 @@ export class HaxSearch extends DDDSuper(I18NMixin(LitElement)) {
     return {
       ...super.properties,
       title: { type: String },
+      siteUrl: { type: String },
+      results: { type: Array },
     };
   }
 
   // Lit scoped styles
   static get styles() {
-    return [super.styles,
-    css`
-      :host {
-        display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
-      }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
-      }
-      h3 span {
-        font-size: var(--hax-search-label-font-size, var(--ddd-font-size-s));
-      }
-    `];
+    return [
+      super.styles,
+      css`
+        :host {
+          display: block;
+          color: var(--ddd-theme-primary);
+          background-color: var(--ddd-theme-accent);
+          font-family: var(--ddd-font-navigation);
+        }
+        .wrapper {
+          margin: var(--ddd-spacing-2);
+          padding: var(--ddd-spacing-4);
+        }
+        h3 span {
+          font-size: var(--hax-search-label-font-size, var(--ddd-font-size-s));
+        }
+
+        .card {
+          background: #f0f0f0;
+          border-radius: 10px;
+          padding: 15px;
+          width: 22%;
+          text-align: left;
+          margin: 10px;
+          display: inline-block;
+        }
+
+        .card img {
+          width: 100%;
+          height: auto;
+          border-radius: 5px;
+        }
+
+        .card a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        .input-area {
+          margin-bottom: 20px;
+        }
+      `,
+    ];
   }
 
   // Lit render the HTML
   render() {
     return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+      <div class="wrapper">
+        <h3><span>${this.t.title}:</span> ${this.title}</h3>
+        <div class="input-area">
+          <input
+            type="text"
+            .value="${this.siteUrl}"
+            @input="${this._updateUrl}"
+            placeholder="Enter HAX site URL"
+          />
+          <button @click="${this._fetchData}">Analyze</button>
+        </div>
+        <div id="results">
+          ${this.results.map((item) => this._renderCard(item))}
+        </div>
+      </div>
+    `;
+  }
+
+  _updateUrl(event) {
+    this.siteUrl = event.target.value;
   }
 
   /**
